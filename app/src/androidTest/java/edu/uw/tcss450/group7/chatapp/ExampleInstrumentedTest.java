b987f3baf7d77783ef28fec994571a6a940f28ce
TroyZon <troyzon@gmail.com>
2021-10-27 16:50:57 -0700

    Inital commit of the project, removed all files connected to lab 1. Replaced with a simple hello world app.

--- app/src/androidTest/java/edu/uw/tcss450/group7/chatapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/edu/uw/tcss450/group7/chatapp/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package edu.uw.tcss450.yavuzalp.lab1myfirstapp;
+package edu.uw.tcss450.group7.chatapp;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("edu.uw.tcss450.yavuzalp.lab1myfirstapp", appContext.getPackageName());
+        assertEquals("edu.uw.tcss450.group7.chatapp", appContext.getPackageName());
     }
 }

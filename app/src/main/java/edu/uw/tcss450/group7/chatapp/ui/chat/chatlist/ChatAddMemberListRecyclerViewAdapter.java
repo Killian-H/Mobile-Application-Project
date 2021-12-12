ae9bc7831fbca6c648ff173512956798d6b342a4
TroyZon <troyzon@gmail.com>
2021-12-12 02:32:44 -0800

    "Add users to chat functionality."

---
+++ app/src/main/java/edu/uw/tcss450/group7/chatapp/ui/chat/chatlist/ChatAddMemberListRecyclerViewAdapter.java
@@ -0,0 +1,88 @@
+package edu.uw.tcss450.group7.chatapp.ui.chat.chatlist;
+
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import java.util.List;
+
+import edu.uw.tcss450.group7.chatapp.R;
+import edu.uw.tcss450.group7.chatapp.databinding.FragmentNewContactCardBinding;
+import edu.uw.tcss450.group7.chatapp.model.ContactsViewModel;
+import edu.uw.tcss450.group7.chatapp.ui.chat.chatroom.ChatViewModel;
+import edu.uw.tcss450.group7.chatapp.ui.contact.Contact;
+
+public class ChatAddMemberListRecyclerViewAdapter extends RecyclerView.Adapter<ChatAddMemberListRecyclerViewAdapter.NewContactViewHolder> {
+
+    //Store all of the blogs to present
+    private final List<Contact> mContacts;
+    private final String mJWT;
+    private ChatViewModel mChatsViewModel;
+    private int mChatId;
+
+
+    public ChatAddMemberListRecyclerViewAdapter(List<Contact> items, ChatViewModel mModel, int chatId, String JWT) {
+        this.mContacts = items;
+        this.mJWT = JWT;
+        this.mChatId = chatId;
+        mChatsViewModel = mModel;
+    }
+
+    @NonNull
+    @Override
+    public NewContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new NewContactViewHolder(LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.fragment_new_contact_card, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NewContactViewHolder holder, int position) {
+        holder.setContact(mContacts.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mContacts.size();
+    }
+
+
+    /**
+     * Objects from this class represent an Individual row View from the List
+     * of rows in the Blog Recycler View.
+     */
+    public class NewContactViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public FragmentNewContactCardBinding binding;
+        private Contact mContact;
+
+        public NewContactViewHolder(View view) {
+            super(view);
+            mView = view;
+            binding = FragmentNewContactCardBinding.bind(view);
+
+        }
+
+
+
+        void setContact(final Contact contact) {
+            mContact = contact;
+
+            binding.textUserDetails.setText(contact.getFirstName() + ", " + contact.getEmail());
+            binding.buttonSendRequest.setText("Add");
+            binding.buttonSendRequest.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mChatsViewModel.connectAddUser(mChatId, mContact.getMemberId(), mJWT);
+                }});
+        }
+    }
+
+}
--- app/src/main/java/edu/uw/tcss450/group7/chatapp/ui/chat/chatroom/ChatViewModel.java
+++ app/src/main/java/edu/uw/tcss450/group7/chatapp/ui/chat/chatroom/ChatViewModel.java
@@ -14,6 +14,7 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -354,5 +355,42 @@ public class ChatViewModel extends AndroidViewModel {
 
         //code here will run
     }
+
+    public void connectAddUser(int chatID, int userID, String jwt) {
+            String url = getApplication().getResources().getString(R.string.base_url) +
+                    "chats/" + chatID +"/";
+
+            HashMap<String, String> params = new HashMap<>();
+            params.put("memberid", "" +userID);
+
+            Request request = new JsonObjectRequest(
+                    Request.Method.PUT,
+                    url,
+                    new JSONObject(params),
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            getChatMembers(chatID,jwt);
+                        }
+                    },
+                    this::handleError) {
+                @Override
+                public Map<String, String> getHeaders() {
+                    Map<String, String> headers = new HashMap<>();
+                    // add headers <key,value>
+                    headers.put("Authorization", jwt);
+                    return headers;
+                }
+            };
+
+            request.setRetryPolicy(new DefaultRetryPolicy(
+                    10_000,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+            //Instantiate the RequestQueue and add the request to the queue
+            Volley.newRequestQueue(getApplication().getApplicationContext())
+                    .add(request);
+    }
+
 
 }
